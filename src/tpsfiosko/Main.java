8ca3f204296dc7a8028a1e2a0f693444b6af7a52
evilwave12 <guille1000@live.com>
2025-12-01 20:16:37 -0300

    me when franco paez with mi balls

--- src/tpsfiosko/Main.java
+++ src/tpsfiosko/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args) 
     {
         ExcelWriter.crearPlanilla();
+        V_principal v = new V_principal();
+        v.setVisible(true);
     }
     
 }
