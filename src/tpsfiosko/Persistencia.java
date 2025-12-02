6d2a1be620b3ef03f0ec456f46af3b60ddf61b98
bernbae <bernabeferreyra78@gmail.com>
2025-12-01 21:20:00 -0300

    Añadida clase Persistencia.

--- src/tpsfiosko/Persistencia.java
+++ src/tpsfiosko/Persistencia.java
@@ -1,6 +1,28 @@
 package tpsfiosko;
 
+import java.util.ArrayList;
+
 public class Persistencia 
 {
+    private ArrayList<Producto> productos;
+    private ArrayList<Venta> Ventas;
     
+    public Persistencia()
+    {
+        inicializarProductos();
+    }
+    
+    public void inicializarProductos()
+    {
+        productos.add(new Producto("Gaseosa 500ml", 750.0f));
+        productos.add(new Producto("Galletitas", 520.0f));
+        productos.add(new Producto("Caramelo", 50.0f));
+        productos.add(new Producto("Chocolate", 890.0f));
+        productos.add(new Producto("Chicle", 80.0f));
+        productos.add(new Producto("Agua mineral 500ml", 600.0f));
+        productos.add(new Producto("Snack salado", 700.0f));
+        productos.add(new Producto("Cigarrillos", 2500.0f));
+        productos.add(new Producto("Alfajor", 650.0f));
+        productos.add(new Producto("Energética 473ml", 1200.0f));
+    }
 }
